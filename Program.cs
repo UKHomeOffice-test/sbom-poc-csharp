9123136233e869d0e80f4092e925d0ebe8ec0edf
SHANMAC\Rob Johnson <robert.johnson@digital.homeoffice.gov.uk>
2022-03-01 14:00:17 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Use Newtonsoft library to outout to my SBOM
+
+var test = @"
+    {
+        'Hello':'World!'
+    }
+";
+
+var jsonTest = Newtonsoft.Json.Linq.JObject.Parse(test);
+
+Console.WriteLine(test);
